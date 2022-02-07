22353ef3f35a899aead7c5a9181b3fa45013179f
Sabin Baniya <dummymail@tutanota.de>
2022-02-07 09:42:14 +0545

    Security packages add

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const connectDB = require("./DB/connectDB");
 const app = express();
 
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+
 require("dotenv").config();
 require("express-async-errors");
 
@@ -10,7 +15,16 @@ const authRouter = require("./Routes/auth");
 const errorHandler = require("./Middlewares/ErrorHandler");
 const Auth = require("./Middlewares/Auth");
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(cors());
 
 app.use("/api/v1/jobs", Auth, jobsRouter);
 app.use("/api/v1/auth", authRouter);
